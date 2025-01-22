207f7e9257f60b23c4d6c55c4095fa2310266e80
nikhil-dev-395 <nikhilw395@gmail.com>
2025-01-23 01:59:15 +0530

    changes the cors

--- backend/api/index.js
+++ backend/api/index.js
@@ -16,14 +16,7 @@ app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(
-  cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true,
-    methods: ["GET", "POST", "PATCH", "DELETE"],
-  })
-);
-
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 if (process.env.NODE_ENV === "DEVELOPMENT") {
   app.use(morgan("dev"));
